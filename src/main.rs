cc0290a7c561c78d0fa59389b51a7c714fcffd00
poultryghast <murp8752@gmail.com>
2021-09-19 00:09:08 -0400

    created huffman tree function for finding prefix codes

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,70 @@
+mod node;
+
+use std::collections::HashMap;
+use std::collections::BinaryHeap;
+use bitvec::prelude::*;
+use node::Node;
+
 fn main() {
     let text = include_bytes!("alice.txt");
     println!("The file is {} bytes.", text.len());
-    let res = compress_rle(text.to_vec()).unwrap();
-    println!("The compressed version is {} bytes.", res.len());
+    let freq = get_freq(text);
+    let res = code_huffman(&freq);
 }
 
-fn compress_rle(input: Vec<u8>) -> Result<Vec<u8>, &'static str> {
+fn compress_rle(input: &[u8]) -> Vec<u8> {
     let mut output : Vec<u8> = Vec::new();
     let mut run = input[0];
     let mut run_len = 0;
     for cur in input {
-        if cur == run {
+        if *cur == run {
             run_len = run_len + 1;
         }
         else {
             output.append(&mut vec![run_len, run]);
-            run = cur;
+            run = *cur;
             run_len = 1;
         }
     }
     output.append(&mut vec![run_len, run]);
-    Ok(output)
+    output
 }
 
+fn get_freq(input: &[u8]) -> HashMap<u8, u32> {
+    let mut map: HashMap<u8, u32> = HashMap::new();
+    for val in input {
+        let entry = map.entry(*val).or_insert(0);
+        *entry = *entry + 1;
+    }
+    map
+}
+
+fn code_huffman(freq: &HashMap<u8, u32>) ->  BitVec {
+    let mut heap: BinaryHeap<Box<Node>> = BinaryHeap::new();
+
+    //Initially populate binary tree with nodes
+    for (key, val) in freq.iter() {
+       let node = Node {
+          left: None,
+          right: None,
+          val: Some(*key),
+          freq: *val
+        };
+        heap.push(Box::from(node));
+    }
+    
+    let root = loop {
+        let i = heap.pop().unwrap();
+        if let Some(j) = heap.pop() {
+            let node = Node {
+                freq: i.freq + j.freq,
+                left: Some(Box::from(i)),
+                right: Some(Box::from(j)),
+                val: None
+            };
+            heap.push(Box::from(node));
+        } else { break i }
+    };
+
+    BitVec::new()
+}
---
+++ src/node.rs
@@ -0,0 +1,31 @@
+use std::cmp::Ord;
+use std::cmp::Ordering;
+
+pub struct Node {
+    pub left: Option<Box<Node>>,
+    pub right: Option<Box<Node>>,
+    pub val: Option<u8>,
+    pub freq: u32
+}
+
+impl Ord for Node {
+    fn cmp(&self, other: &Self) -> Ordering {
+        //Backwards ordering for min heap
+        other.val.cmp(&self.val)
+    }
+}
+
+impl PartialOrd for Node {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for Node {
+    fn eq(&self, other: &Self) -> bool {
+        self.val == other.val
+    }
+}
+
+impl Eq for Node {}
+
