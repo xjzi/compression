fba52a190d5fe7100e9da98d3b4b5164bd46acb8
poultryghast <murp8752@gmail.com>
2021-10-08 20:22:03 -0400

    updated namespaces for benchmarking

--- benches/huffman.rs
+++ benches/huffman.rs
@@ -1,11 +1,11 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use compression::huffman;
+use compression;
 
 fn criterion_benchmark(c: &mut Criterion) {
     let text = include_bytes!("../corpus/alice29.txt");
-    let compressed = huffman::compress::compress(text);
-    c.bench_function("huffman compress alice", |b| b.iter(|| huffman::compress::compress(black_box(text))));
-    c.bench_function("huffman decompress alice", |b| b.iter(|| huffman::decompress::decompress(black_box(compressed.clone()))));
+    let compressed = compression::compress::compress(text);
+    c.bench_function("huffman compress alice", |b| b.iter(|| compression::compress::compress(black_box(text))));
+    c.bench_function("huffman decompress alice", |b| b.iter(|| compression::decompress::decompress(black_box(compressed.clone()))));
 }
 
 criterion_group!(benches, criterion_benchmark);
