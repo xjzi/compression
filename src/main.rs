e2613a1fa779e253b46dda0acd14a56eb2be2a21
poultryghast <murp8752@gmail.com>
2021-10-10 18:11:51 -0400

    wrapped in a better cli

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,66 @@
 use compression;
-use std::io::{self, Write, Read};
-use std::env;
+use std::io::{self, Read, Write};
+use std::fs::File;
+
+#[macro_use]
+extern crate clap;
+use clap::App;
 
 #[cfg(test)]
 mod test;
 
-fn main() {
-    let mut buffer: Vec<u8> = Vec::new();
-    io::stdin().read_to_end(&mut buffer).ok();
-    let args: Vec<String> = env::args().collect();
-
-    match &args[1][..] {
-        "c" => compress(&buffer),
-        "d" => decompress(buffer),
-        _ => println!("Not an option.")
+fn input(input: Option<&str>) -> Result<Vec<u8>, io::Error> {
+    if let Some(filename) = input {
+        let mut file = File::open(filename)?;
+        let mut buf: Vec<u8> = Vec::new();
+        file.read_to_end(&mut buf)?;
+        Ok(buf)
+    } else {
+        let mut buf: Vec<u8> = Vec::new();
+        io::stdin().read_to_end(&mut buf)?;
+        Ok(buf)
     }
 }
 
-fn compress(bytes: &[u8]) {
-    let bits = compression::compress::compress(bytes);
-    let file = compression::wrap_bits(bits);
-    io::stdout().write_all(&file).ok();
+fn output(output: Option<&str>) -> Result<Box<dyn Write>, io::Error> {
+    if let Some(filename) = output {
+        let file = File::create(filename)?;
+        Ok(Box::from(file))
+    } else {
+        let out = io::stdout();
+        Ok(Box::from(out))
+    }
 }
 
-fn decompress(bytes: Vec<u8>) {
+fn main() {
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+
+    let input = input(matches.value_of("input-file")).unwrap();
+    let output = output(matches.value_of("output-file")).unwrap();
+   
+    if matches.is_present("compress") {
+        compress(&input, output);
+    } else if matches.is_present("decompress") {
+        decompress(input, output);
+    } 
+
+    //match matches.subcommand_name() {
+    //    Some("c") => compress(&input, output),
+    //    Some("d") => decompress(input, output),
+    //    _ => {}
+    //}
+}
+
+fn compress(bytes: &[u8], mut output: Box<dyn Write>) {
+    let bits = compression::compress::compress(bytes);
+    let file = compression::wrap_bits(bits);
+    output.write_all(&file).ok();
+}
+
+fn decompress(bytes: Vec<u8>, mut output: Box<dyn Write>) {
     let bits = compression::unwrap_bytes(bytes);
     let bytes = compression::decompress::decompress(bits);
-    io::stdout().write_all(&bytes).ok();
+    output.write_all(&bytes).ok();
 }
 
